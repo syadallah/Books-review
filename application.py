fd72bdc21e7dd3f575ce22126d70454860fb1a99
syadallah <syadallah@gmail.com>
2020-04-03 17:20:46 -0400

    Test the App

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-import requests
+# import requests
 
 from flask import Flask, session
 from flask_session import Session
@@ -15,7 +15,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
@@ -24,5 +24,7 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "5fI1wLvZOj028LGUwMrLdQ",
-                                                                                     "isbns": "0441172717"}).json()
+    ##    res=requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "5fI1wLvZOj028LGUwMrLdQ",
+    ##                                                                                  "isbns": "1632168146"}).json()
+    ##    print(res)
+    return 'Python APP'
